943c2db25be2d62657d5b64a7f4d1f8d6fc946d2
Leo Ribeiro <leonardo@squareup.com>
2023-09-26 18:18:27 -0400

    fix tests

--- __tests__/docs-report.test.ts
+++ __tests__/docs-report.test.ts
@@ -2,7 +2,7 @@ import { generateReport } from '../src/docs-report'
 
 describe('docs report', () => {
   it('generates an api-extractor report', async () => {
-    const report = await generateReport('api-extractor', './api-extractor.json')
+    const report = await generateReport('api-extractor')
     expect(report.errorsCount).toBe(0)
     expect(report.messages).toHaveLength(13)
   })
