d416f0be3e8fcdd579d5c796be4270e802f7b52a
Leo Ribeiro <leordev@gmail.com>
2023-09-28 14:22:19 -0400

    fix build

--- __tests__/run.test.ts
+++ __tests__/run.test.ts
@@ -6,12 +6,14 @@
  * variables following the pattern `INPUT_<INPUT_NAME>`.
  */
 
-import * as core from '@actions/core'
 import * as run from '../src/run'
 import * as docsReport from '../src/docs-report'
 
-const debugMock = jest.spyOn(core, 'debug')
-const setFailedMock = jest.spyOn(core, 'setFailed')
+jest.mock('@actions/core')
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires, import/no-commonjs, @typescript-eslint/no-require-imports
+const core = require('@actions/core')
+
 const runDocsReportMock = jest.spyOn(docsReport, 'runDocsReport')
 const runMock = jest.spyOn(run, 'run')
 
@@ -52,8 +54,8 @@ describe('action', () => {
     await run.run()
     expect(runMock).toHaveReturned()
     expect(runDocsReportMock).toHaveBeenCalledTimes(1)
-    expect(debugMock).toHaveBeenCalledTimes(1)
-    expect(setFailedMock).toHaveBeenCalledTimes(0)
+    expect(core.debug).toHaveBeenCalledTimes(1)
+    expect(core.setFailed).toHaveBeenCalledTimes(0)
   })
 
   it('sets a failed status', async () => {
@@ -65,6 +67,6 @@ describe('action', () => {
     expect(runMock).toHaveReturned()
 
     // Verify that all of the core library functions were called correctly
-    expect(setFailedMock).toHaveBeenNthCalledWith(1, 'failed')
+    expect(core.setFailed).toHaveBeenNthCalledWith(1, 'failed')
   })
 })
