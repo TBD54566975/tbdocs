52c55c19d080c23a0f2c571a20538e03352f4e08
Leo Ribeiro <leordev@gmail.com>
2023-09-28 09:04:54 -0400

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-var lib = require('../dist/index.js')
+var lib = require(path.join(__dirname, '../dist/index.js'))
 
 // list all files in the current working directory
 console.log(require('fs').readdirSync('.'))
