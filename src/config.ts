dbfe879c88e93cd42fb6f18670aba7ad2b80f167
Leo Ribeiro <leonardo@squareup.com>
2023-09-28 21:33:12 -0400

    docs-generator push to git wip

--- src/config.ts
+++ src/config.ts
@@ -2,10 +2,14 @@ import { getInput } from '@actions/core'
 import { DocsReporterType, DocsGeneratorType } from './docs-report'
 
 export const configInputs = {
+  projectPath: getInput('project_path') || '.',
   docsReport: (getInput('docs_report') || '') as DocsReporterType,
   docsGenerator: (getInput('docs_generator') || '') as DocsGeneratorType,
+  docsTargetOwnerRepo: getInput('docs_target_owner_repo'),
+  docsTargetBranch: getInput('docs_target_branch'),
+  docsTargetPrBaseBranch: getInput('docs_target_pr_base_branch'),
+  docsTargetRepoPath: getInput('docs_target_repo_path'),
   failOnError: getInput('fail_on_error') === 'true',
-  projectPath: getInput('project_path') || '.',
   token: getInput('token'),
   apiExtractor: {
     jsonPath: getInput('api_extractor_json_path')
--- src/docs-generator.ts
+++ src/docs-generator.ts
@@ -1,6 +1,7 @@
 import { Application } from 'typedoc'
 import path from 'path'
-import { existsSync } from 'fs'
+import { existsSync, readFileSync, readdirSync } from 'fs'
+import * as github from '@actions/github'
 
 import { configInputs } from './config'
 
@@ -16,9 +17,13 @@ const GENERATED_DOCS_DIR = path.join(configInputs.projectPath, '.tbdocs/docs')
 export const generateDocs = async (): Promise<void> => {
   switch (configInputs.docsGenerator) {
     case 'typedoc-markdown':
-      return generateTypedocMarkdown()
+      await generateTypedocMarkdown()
     default:
       throw new Error(`Unknown docs generator: ${configInputs.docsGenerator}`)
+  }
+
+  if (configInputs.docsTargetOwnerRepo && configInputs.docsTargetBranch) {
+    await openPr()
   }
 }
 
@@ -46,4 +51,158 @@ const generateTypedocMarkdown = async (): Promise<void> => {
   }
 
   await generatorApp.generateDocs(projectReflection, GENERATED_DOCS_DIR)
+}
+
+/**
+ * Open a PR to Github with the generated docs
+ */
+const openPr = async (): Promise<void> => {
+  console.log('>>> Opening PR...')
+
+  const octokit = github.getOctokit(configInputs.token)
+  const [targetOwner, targetRepo] = configInputs.docsTargetOwnerRepo.split('/')
+  const targetBranch = configInputs.docsTargetBranch
+  const targetBase = configInputs.docsTargetPrBaseBranch
+  const targetRepoPath = configInputs.docsTargetRepoPath
+
+  if (
+    !targetOwner ||
+    !targetRepo ||
+    !targetBranch ||
+    !targetBase ||
+    !targetRepoPath
+  ) {
+    throw new Error(
+      `Missing targetOwner, targetRepo, targetBranch, targetBase, or targetRepoPath`
+    )
+  }
+
+  console.info('Open PR data', {
+    targetOwner,
+    targetRepo,
+    targetBranch,
+    targetBase,
+    targetRepoPath
+  })
+
+  await createBranch(octokit, targetOwner, targetRepo, targetBranch, targetBase)
+  await pushDocsToBranch(
+    octokit,
+    targetOwner,
+    targetRepo,
+    targetBranch,
+    targetRepoPath
+  )
+
+  // await createOrUpdatePr(
+  //   octokit,
+  //   targetOwner,
+  //   targetRepo,
+  //   targetBranch,
+  //   targetBase,
+  //   targetRepoPath
+  // )
+}
+
+/**
+ * Check if the branch exists, if not create it
+ */
+const createBranch = async (
+  octokit: ReturnType<typeof github.getOctokit>,
+  targetOwner: string,
+  targetRepo: string,
+  targetBranch: string,
+  targetBase: string
+) => {
+  console.info(`>>> Checking if branch ${targetBranch} exists...`)
+  const branch = await octokit.rest.repos.getBranch({
+    owner: targetOwner,
+    repo: targetRepo,
+    branch: targetBranch
+  })
+
+  if (!branch) {
+    console.info(`>>> Branch ${targetBranch} does not exist. Creating...`)
+
+    console.info(`>>> Getting base branch ${targetBase} sha...`)
+    const { data: baseBranchData } = await octokit.rest.repos.getBranch({
+      owner: targetOwner,
+      repo: targetRepo,
+      branch: targetBase
+    })
+
+    const baseSha = baseBranchData.commit.sha
+    console.info(
+      `>>> Creating branch ${targetBranch} with sha ${baseSha.slice(0, 7)}...`
+    )
+    await octokit.rest.git.createRef({
+      owner: targetOwner,
+      repo: targetRepo,
+      ref: `refs/heads/${targetBranch}`,
+      sha: baseSha
+    })
+  }
+}
+
+const pushDocsToBranch = async (
+  octokit: ReturnType<typeof github.getOctokit>,
+  targetOwner: string,
+  targetRepo: string,
+  targetBranch: string,
+  targetRepoPath: string
+) => {
+  const files = readdirSync(GENERATED_DOCS_DIR, { recursive: true }) as string[]
+
+  const blobs = await Promise.all(
+    files.map(async file => {
+      const content = readFileSync(path.join(GENERATED_DOCS_DIR, file))
+      const blobData = await octokit.rest.git.createBlob({
+        owner: targetOwner,
+        repo: targetRepo,
+        content: content.toString('base64'),
+        encoding: 'base64'
+      })
+
+      return {
+        path: path.join(targetRepoPath, file),
+        mode: '100644' as '100644',
+        type: 'blob' as 'blob',
+        sha: blobData.data.sha
+      }
+    })
+  )
+
+  const { data: refData } = await octokit.rest.git.getRef({
+    owner: targetOwner,
+    repo: targetRepo,
+    ref: `heads/${targetBranch}`
+  })
+  const commitSHA = refData.object.sha
+  const { data: commitData } = await octokit.rest.git.getCommit({
+    owner: targetOwner,
+    repo: targetRepo,
+    commit_sha: commitSHA
+  })
+
+  const { data: treeData } = await octokit.rest.git.createTree({
+    owner: targetOwner,
+    repo: targetRepo,
+    tree: blobs,
+    base_tree: commitData.tree.sha
+  })
+
+  const { data: newCommitData } = await octokit.rest.git.createCommit({
+    owner: targetOwner,
+    repo: targetRepo,
+    message: `tbdocs: committing generated docs files ${github.context.runId}-${github.context.runNumber}`,
+    tree: treeData.sha,
+    parents: [commitSHA]
+  })
+
+  await octokit.rest.git.updateRef({
+    owner: targetOwner,
+    repo: targetRepo,
+    ref: `heads/${targetBranch}`,
+    sha: newCommitData.sha
+  })
 }
